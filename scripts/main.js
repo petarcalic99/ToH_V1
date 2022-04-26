da895613b12789173c20717efe799269334a37d0
pcalic-ledger <petar.calic@ledger.fr>
2022-04-26 16:44:10 +0200

    first toy site

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+let myImage = document.querySelector('img');
+//Switch pictures on click
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc === 'images/eth.jpg') {
+    myImage.setAttribute ('src','images/eth.jpg');
+  } else {
+    myImage.setAttribute ('src','images/eth.jpg');
+  }
+}
+
+// Personalized welcome message code
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Welcome ' + myName;
+  }
+}
+	//Stored name 
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Welcome ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
